fb814e5d658fa4604867c4e20c4d509b1deaab52
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-07-01 06:00:45 -0500

    auth screens export file

---
+++ screens/auth/authExports.js
@@ -0,0 +1,5 @@
+import LandingScreen from "./landingScreen";
+import LoginScreen from "./loginScreen";
+import SignupScreen from "./signupScreen";
+
+export { LandingScreen, LoginScreen, SignupScreen };
