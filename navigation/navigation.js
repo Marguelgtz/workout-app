7324462f1f337078833f00318a5a8a7dfb0ea602
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-07-01 05:02:31 -0500

    initial pseudo code fro navigation of app

---
+++ navigation/navigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+//import navigators
+import {
+  createSwitchNavigator,
+  createAppContainer,
+  createStackNavigator,
+} from "react-navigation";
+
+// screen imports
+/* 
+-- AUTH 
+Auth loading screen 
+landing screen 
+sign up screen 
+login screen
+
+--APP BOTTOM TABS
+homescreen (routines)
+general stats 
+profile 
+
+-- OFF TAB SCREENS 
+create routine 
+exersices 
+exersice 
+routine 
+active routine thing
+*/
